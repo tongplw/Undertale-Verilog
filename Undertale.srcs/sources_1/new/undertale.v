ade9d16b5a1cc3c000691a4a14d07dabc6687061
tongplw <bangkok.tong@gmail.com>
2020-05-23 02:21:34 +0700

    fix vga module

--- Undertale.srcs/sources_1/new/undertale.v
+++ Undertale.srcs/sources_1/new/undertale.v
@@ -31,21 +31,25 @@ module undertale(
 //    reg clk = 0;
 //    always #1 clk = ~clk;
     
-    wire page_num, ena;
+    wire page_num, ena, de;
     wire up, left, down, right, space;
     wire [7:0] command;
     wire [11:0] x, y;
     wire [2:0] intro_rgb, game_rgb, selected_rgb;
+    wire [11:0] rgb;
     
     // Read Image File
     intro_page intro_page(page_num, x, y, intro_rgb);
     game_page game_page(clk, page_num, x, y, game_rgb, up, left, down, right, space);
-
+//    color_decode color_decode(selected_rgb, rgb);
+    
     assign selected_rgb = (page_num == 0) ? intro_rgb : game_rgb;
+    assign {vgaRed, vgaGreen, vgaBlue} = rgb;
+    assign rgb = (de) ? 12'h0f0 : 12'h000;
     
     // ---------------------------------------------------------------------------
     controller controller(clk, command, ena, page_num, up, down, left, right, space);
-    vga vga(clk, Hsync, Vsync, {vgaRed, vgaGreen, vgaBlue}, x, y, selected_rgb);
+    vga vga(clk, Hsync, Vsync, x, y, de);
     uart uart(clk, RsRx, RsTx, command, ena);
     
 endmodule
--- Undertale.srcs/sources_1/new/vga.v
+++ Undertale.srcs/sources_1/new/vga.v
@@ -23,45 +23,38 @@
 module vga(
     input clk,
     output Hsync, Vsync,
-    output [11:0] rgb,
     output [11:0] x, y,
-    input [2:0] encoded_rgb
+    output de
     );
     
-    // Custom Clock Divider
-//    reg new_clk = 0;
-//    integer n = 0;
-//    always @(posedge clk) begin
-//        n = n + 1;
-//        if (n == 2) begin
-//            n = 0;
-//            new_clk = ~new_clk;
-//        end
-//    end
-
+    // clock divider from 100 MHz to 25 MHz    
     reg [15:0] cnt;
     reg pix_stb; 
     always @(posedge clk)
         {pix_stb, cnt} <= cnt + 16'h4000;  // divide by 4: (2^16)/4 = 0x4000
 
-
     parameter WIDTH = 640;
     parameter HEIGHT = 480;
-    parameter H_FP = 16 , H_SYNC =96 , H_BP = 48;
-    parameter V_FP = 10 , V_SYNC =2 , V_BP = 33;
+    parameter H_FP = 16, H_SYNC = 96, H_BP = 48;
+    parameter V_FP = 10, V_SYNC = 2, V_BP = 33;
     parameter H_TOTAL = 800;
     parameter V_TOTAL = 525;
+
+    parameter HS_START = WIDTH + H_FP;
+    parameter HS_END = WIDTH + H_FP + H_SYNC;
+    parameter VS_START = HEIGHT + V_FP;
+    parameter VS_END = HEIGHT + V_FP + V_SYNC;
+    
+//    parameter HA_START = H_FP + H_SYNC + H_BP;
+//    parameter VA_END = HEIGHT;
     
     reg line_clk = 0;
     reg [9:0] h_val = 0, v_val = 0;
     
-//    color_decode color_decode(encoded_rgb, rgb);
-    assign rgb = 12'h0f0;
-    
-    assign x = (h_val >= H_BP + H_SYNC + H_BP) ? (h_val - H_BP - H_SYNC - H_BP) / 4 : 0;
-    assign y = (v_val >= V_BP + V_SYNC + V_BP) ? (v_val - V_BP - V_SYNC - V_BP) / 4 : 0;
-    assign Hsync = ~((h_val >= H_FP) & (h_val < H_FP + H_SYNC));
-    assign Vsync = ~((v_val >= V_FP) & (v_val < V_FP + V_SYNC));
+    assign x = (h_val < WIDTH) ? h_val / 4 : 0;
+    assign y = (v_val < HEIGHT) ? v_val / 4 : 0;
+    assign Hsync = (h_val <= HS_START) || (h_val >= HS_END);
+    assign Vsync = (v_val <= VS_START) || (v_val >= VS_END);
 
     // HSYNC
     always @(posedge pix_stb)
@@ -76,5 +69,8 @@ module vga(
         if (v_val >= V_TOTAL) v_val = 0;
         else v_val = v_val + 1;
     end
+    
+    // DISPLAY ENABLE
+    assign de = (h_val < H_TOTAL) && (v_val < V_TOTAL);
     
 endmodule
