a8fa8608e28f87698be4dc2dd89a1b475a7c0789
tongplw <bangkok.tong@gmail.com>
2020-05-22 23:47:56 +0700

    hope it works

--- Undertale.runs/synth_1/undertale.tcl
+++ Undertale.runs/synth_1/undertale.tcl
@@ -17,6 +17,7 @@ proc create_report { reportName command } {
     send_msg_id runtcl-5 warning "$msg"
   }
 }
+set_param xicom.use_bs_reader 1
 set_msg_config -id {Common 17-41} -limit 10000000
 create_project -in_memory -part xc7a35tcpg236-1
 
@@ -32,7 +33,6 @@ set_property ip_output_repo c:/Users/tongplw/Desktop/Undertale/Undertale.cache/i
 set_property ip_cache_permissions {read write} [current_project]
 read_mem C:/Users/tongplw/Desktop/Undertale/Undertale.srcs/sources_1/new/intro.list
 read_verilog -library xil_defaultlib {
-  C:/Users/tongplw/Desktop/Undertale/Undertale.srcs/sources_1/new/color_decode.v
   C:/Users/tongplw/Desktop/Undertale/Undertale.srcs/sources_1/new/controller.v
   C:/Users/tongplw/Desktop/Undertale/Undertale.srcs/sources_1/new/game_page.v
   C:/Users/tongplw/Desktop/Undertale/Undertale.srcs/sources_1/new/intro_page.v
--- Undertale.srcs/sources_1/new/vga.v
+++ Undertale.srcs/sources_1/new/vga.v
@@ -28,25 +28,43 @@ module vga(
     input [2:0] encoded_rgb
     );
     
-    parameter WIDTH = 1920;
-    parameter HEIGHT = 1080;
-    parameter H_BACK_PORCH = 148;
-    parameter V_BACK_PORCH = 36;
-    parameter H_TOTAL = 2200;
-    parameter V_TOTAL = 1125;
+    // Custom Clock Divider
+//    reg new_clk = 0;
+//    integer n = 0;
+//    always @(posedge clk) begin
+//        n = n + 1;
+//        if (n == 2) begin
+//            n = 0;
+//            new_clk = ~new_clk;
+//        end
+//    end
+
+    reg [15:0] cnt;
+    reg pix_stb; 
+    always @(posedge clk)
+        {pix_stb, cnt} <= cnt + 16'h4000;  // divide by 4: (2^16)/4 = 0x4000
+
+
+    parameter WIDTH = 640;
+    parameter HEIGHT = 480;
+    parameter H_FP = 16 , H_SYNC =96 , H_BP = 48;
+    parameter V_FP = 10 , V_SYNC =2 , V_BP = 33;
+    parameter H_TOTAL = 800;
+    parameter V_TOTAL = 525;
     
     reg line_clk = 0;
-    reg [15:0] h_val = 0, v_val = 0;
+    reg [9:0] h_val = 0, v_val = 0;
     
-    color_decode color_decode(encoded_rgb, rgb);
+//    color_decode color_decode(encoded_rgb, rgb);
+    assign rgb = 12'h0f0;
     
-    assign x = (h_val >= H_BACK_PORCH & h_val < WIDTH + H_BACK_PORCH) ? (h_val - H_BACK_PORCH) / 4 : 0;
-    assign y = (v_val >= V_BACK_PORCH & v_val < HEIGHT + V_BACK_PORCH) ? (v_val - V_BACK_PORCH) / 4 : 0;
-    assign Hsync = (h_val < WIDTH + H_BACK_PORCH) ? 1 : 0;
-    assign Vsync = (v_val < HEIGHT + V_BACK_PORCH) ? 1 : 0;
+    assign x = (h_val >= H_BP + H_SYNC + H_BP) ? (h_val - H_BP - H_SYNC - H_BP) / 4 : 0;
+    assign y = (v_val >= V_BP + V_SYNC + V_BP) ? (v_val - V_BP - V_SYNC - V_BP) / 4 : 0;
+    assign Hsync = ~((h_val >= H_FP) & (h_val < H_FP + H_SYNC));
+    assign Vsync = ~((v_val >= V_FP) & (v_val < V_FP + V_SYNC));
 
     // HSYNC
-    always @(posedge clk)
+    always @(posedge pix_stb)
     begin
         if (h_val >= H_TOTAL) begin h_val = 0; line_clk = 1; end
         else begin h_val = h_val + 1; line_clk = 0; end
