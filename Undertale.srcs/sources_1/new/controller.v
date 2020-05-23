a8e471da79574cf2ab6929ee05d6115c2c60c9de
tongplw <bangkok.tong@gmail.com>
2020-05-24 01:22:37 +0700

    red dot now can move!!

--- Undertale.srcs/sources_1/new/controller.v
+++ Undertale.srcs/sources_1/new/controller.v
@@ -29,29 +29,37 @@ module controller(
     );
     
     reg change_page = 0;
+    reg push = 0;
     initial page_num = 0;
     
     always @(posedge clk) begin
-        if (ena == 1)
+        if (ena == 1 && push == 0) begin
+            push = 1;
             case(command)
-                8'h10 : change_page = 1;        // ENTER_KEY
+                8'h0D : change_page = 1;        // ENTER_KEY
                 8'h20 : space = 1;              // SPACE_KEY
                 8'h77 : up = 1;                 // W_KEY
-                8'h61 : left = 1;               // A_KEY
-                8'h73 : down = 1;               // S_KEY
+                8'h73 : left = 1;               // A_KEY
+                8'h61 : down = 1;               // S_KEY
                 8'h64 : right = 1;              // D_KEY          
             endcase
-        else begin
+        end
+        else if (ena == 1 && push == 1) begin
             up = 0;
             left = 0;
             down = 0;
             right = 0;
             space = 0;
+        end
+        else begin
+            push = 0;
+            
+            // change page only when displaying nothing
             if (~de && change_page) begin
                 change_page = 0;
                 page_num = ~page_num;
             end
-        end 
+        end
 
     end
     
