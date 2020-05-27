3ef6a5cd3dcbe20ddf48a11559e9611dbe640305
Peeranuth Kehasukcharoen <peeranuth.k@gmail.com>
2020-05-27 13:52:23 +0700

    :checkered_flag: damage scale fixed and reset guage on entering fight
    :hankey: project is no more

--- Undertale.srcs/sources_1/new/calculate_score.v
+++ Undertale.srcs/sources_1/new/calculate_score.v
@@ -21,14 +21,19 @@
 
 
 module calculate_score(
+        input clk,
         input [11:0] x,
         input move_enable,
         output reg [5:0] damage
     );
-    always @(x) begin
-        if(x > 315 && x < 325) damage = 5;
-        else if (x > 305 && x < 335) damage = 3;
-        else if (x > 295 && x < 345) damage = 1;
-        else damage = 0;
+    always @(posedge clk) begin
+        if (!move_enable) begin
+            if(x > 315 && x < 325) begin damage <= 5; end
+            else if (x > 305 && x < 335) begin damage <= 3; end
+            else if (x > 295 && x < 345) begin damage <= 1; end
+            else begin damage <= 0; end
+        end
+        else 
+            damage <= 0;     // just in case, to prevent error
     end
 endmodule
